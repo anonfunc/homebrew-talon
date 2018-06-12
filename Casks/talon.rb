a0e1ed7cd0bac626a000d646c5237cacf26edf3b
anonfunc <anonfunc@gmail.com>
2018-06-11 17:23:47 -0700

    Adding talon cask.

---
+++ Casks/talon.rb
@@ -0,0 +1,10 @@
+cask 'talon' do
+  version '34-0.0.6.16'
+  sha256 '7924e37155628ff848d69ebc817bf33c616fb42455126f3855c8a58fe4980c57'
+
+  url 'https://talonvoice.com/update/Talon-34-0.0.6.16.dmg'
+  name 'Talon'
+  homepage 'https://talonvoice.com/'
+
+  app 'Talon.app'
+end
